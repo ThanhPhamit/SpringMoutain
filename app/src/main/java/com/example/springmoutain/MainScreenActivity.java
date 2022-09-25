afd4d040d001cd97cd9bb88e032ceda4c02acb70
PhamVanThanhG <robertthanh1107@gmail.com>
2022-09-25 22:42:16 +0700

    create_main_screen

--- app/src/main/java/com/example/springmoutain/MainScreenActivity.java
+++ app/src/main/java/com/example/springmoutain/MainScreenActivity.java
@@ -1,16 +1,14 @@
 package com.example.springmoutain;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 }
